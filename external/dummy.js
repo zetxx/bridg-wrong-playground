c587883384ad69c90ba5458fff76fa0bdc8496be
zetxx <zetxx@belogradchik.biz>
2019-04-22 14:54:33 +0300

    feat: dummy external

---
+++ external/dummy.js
@@ -0,0 +1,22 @@
+module.exports = (Node) => {
+    class Http extends Node {
+        triggerEvent(event, message = {}) {
+            this.log('info', {in: 'triggerEvent', event, message});
+            return this.findExternalMethod({method: `event.${event}`})
+                .then((fn) => fn(this.getInternalCommunicationContext({direction: 'in'}), message, {}))
+                .then((result) => this.externalOut({result, meta: {method: event, event: true}}))
+                .catch((error) => this.log('error', {in: 'method:triggerEvent', error}));
+        }
+
+        externalOut({result, error, meta}) {
+            this.log('info', {in: 'externalOut', message: result, error, meta});
+            let newMeta = {...meta};
+            if (meta && meta.event) {
+                newMeta = {method: [meta.method, 'response'].join('.')};
+            }
+            return this.externalIn({result: result, meta: newMeta});
+        }
+    }
+
+    return Http;
+};
