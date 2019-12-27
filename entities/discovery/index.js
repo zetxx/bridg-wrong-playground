e3eee45c52f286b51b2ed39b1f0a5d8f087c0e5c
zetxx <zetxx@belogradchik.biz>
2019-12-27 18:32:51 +0200

    fix: common logic in discovery

---
+++ entities/discovery/index.js
@@ -0,0 +1,11 @@
+module.exports = (Node) => {
+    class ApiDiscovery extends Node {
+        async remoteApiRequest({destination, message, meta}) {
+            var [nodeName, ...rest] = destination.split('.');
+            this.log('trace', {in: 'discovery.remoteApiRequest', description: `try to call micro-service: ${destination}`, destination, message, meta});
+            let request = await this.resolve(nodeName);
+            return request({method: rest.join('.'), params: (message || {}), meta: Object.assign({}, meta, {source: this.getNodeId(), destination})});
+        }
+    }
+    return ApiDiscovery;
+};
