3b998f5c45fc100a3b435fff0139ac714a25408a
zetxx <zetxx@belogradchik.biz>
2020-01-15 17:21:05 +0200

    fix: errors

--- error.js
+++ error.js
@@ -1,14 +1,14 @@
 const flatten = require('flat');
 
-const CreateError = (code, parentError) => {
+const CreateError = ({code, parentError, message}) => {
     class CustomError extends (parentError || Error) {
-        constructor({message, state} = {message: '', state: null}, ...params) {
-            super(message, ...params);
+        constructor({state} = {state: null}, ...params) {
+            super(...params);
             // Maintains proper stack trace for where our error was thrown (only available on V8)
             Error.captureStackTrace && Error.captureStackTrace(this, CustomError);
 
             this.code = (this.code && [this.code, code].join('.')) || code;
-            this.state = flatten(state);
+            this.state = state && flatten(state);
         }
     }
     return CustomError;
