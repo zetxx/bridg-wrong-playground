48f8a7467b26a7e9187a411d20c165f5457ad85e
zetxx <zetxx@belogradchik.biz>
2020-01-15 15:49:12 +0200

    feat: errors

---
+++ error.js
@@ -0,0 +1,15 @@
+const CreateError = (code, parentError) => {
+    class CustomError extends (parentError || Error) {
+        constructor({message, state} = {message: '', state: null}, ...params) {
+            super(message, ...params);
+            // Maintains proper stack trace for where our error was thrown (only available on V8)
+            Error.captureStackTrace && Error.captureStackTrace(this, CustomError);
+
+            this.code = (this.code && [this.code, code].join('.')) || code;
+            this.state = state;
+        }
+    }
+    return CustomError;
+};
+
+module.exports = CreateError;
